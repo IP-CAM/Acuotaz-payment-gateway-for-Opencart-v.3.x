9e21ea9d707b164e7633acdc5aa9af387cea6ccc
Rodrigo Pulcha <rodrigo@apurata.com>
2020-09-21 15:18:32 -0500

    Create directory for installation

---
+++ upload/admin/language/en-gb/extension/payment/apurata.php
@@ -0,0 +1,29 @@
+<?php
+// Heading
+$_['heading_title']        = 'Apurata';
+
+// Text
+$_['text_extension']       = 'Extensions';
+$_['text_success']         = 'Success: You have modified Apurata details!';
+$_['text_edit']            = 'Edit Apurata';
+
+// Entry
+$_['entry_apurata']        = 'Apurata Instructions';
+$_['entry_apurata_id']     = 'ID Apurata';
+$_['entry_apurata_secret'] = 'Token secreto';
+$_['entry_order_status']   = 'Order Status';
+$_['entry_geo_zone']       = 'Geo Zone';
+$_['entry_status']         = 'Status';
+$_['entry_sort_order']     = 'Sort Order';
+$_['entry_client_id']      = 'ID del cliente';
+$_['entry_client_secret']  = 'Secreto del cliente';
+$_['entry_allow_http']     = 'Permitir conexiones http';
+
+// Help
+$_['help_apurata_id']      = 'La identificacion del ecommerce obtenida en el onboarding con Apurata.';
+$_['help_apurata_secret']  = 'El token secreto del ecommerce obtenido en el onboarding con Apurata.'; 
+
+// Error 
+$_['error_permission']     = 'Warning: You do not have permission to modify payment apurata!';
+$_['error_bank']           = 'Apurata Instructions Required!';
+$_['apurata_api_domain']   = 'http://localhost:8000';
